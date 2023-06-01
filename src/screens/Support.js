ac322996d0c830bafc444d105c9ce557c4241e8a
MohamedUwk22 <mohamed.uwk2200@gmail.com>
2023-06-01 15:40:33 +0300

    Support and Update page changes

--- src/screens/Support.js
+++ src/screens/Support.js
@@ -1,21 +1,43 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React,{useState} from "react";
+import { StyleSheet, Text, View ,TextInput} from "react-native";
 import Background from "../components/Background";
 import MainBar from "../components/MainBar";
 import Header from "../components/Header";
+import Button from "../components/Button";
+
 import { useTranslation } from "react-i18next";
 
 export default function Support() {
   const { t, i18n } = useTranslation();
+  const [feedback, setFeedback] = useState("");
+
+  const handleSubmit = () => {
+     console.log("Feedback submitted:", feedback);
+     setFeedback("");
+  };
 
   return (
     <Background>
       <Header />
-        <View style={styles.container}>
-          <Text style={styles.text}>
-            {t(`support.notAvailable`)}
-          </Text>
-        </View>
+      <View style={styles.container}>
+      <Text style={styles.title}>{t(`SupportScreen.feedbackTitle`)}
+      </Text>
+      <Text style={styles.label}>{t(`SupportScreen.feedbackLabel`)}</Text>
+      <TextInput
+        placeholder={t(`SupportScreen.feedback`)}
+        style={styles.input}
+        multiline
+        numberOfLines={4}
+        value={feedback}
+        onChangeText={setFeedback}
+      />
+       <Button
+         mode="contained"
+        onPress={handleSubmit}
+      >
+        {t(`SupportScreen.sendFeedback`)}
+      </Button>
+      </View>
       <MainBar />
     </Background>
   );
@@ -24,13 +46,27 @@ export default function Support() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center'
-  },
-  text: {
-    fontFamily: 'regularFont',
-    fontSize: 20,
-    backgroundColor: '#fff',
     padding: 20,
-    marginHorizontal: 10,
-  }
+    
+  },
+  title: {
+    flex:0.4,
+    color:"white",
+    fontSize: 30,
+    fontWeight: "bold",
+    marginBottom: 10,
+    textAlign:'center',
+  },
+  label: {
+    color:"white",
+    fontWeight: "bold",
+    marginBottom: 5,
+  },
+  input: {
+    borderWidth: 1,
+    padding: 10,
+    marginBottom: 20,
+    borderRadius: 5,
+    textAlignVertical: "top",
+  },
 });
